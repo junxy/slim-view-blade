918bf81ab5d293aa1a7773f1170156dda435f205
Junior <ayijun@gmail.com>
2015-05-17 23:54:43 +0800

    added a blade template

--- public/index.php
+++ public/index.php
@@ -1,10 +1,23 @@
 <?php
 
-require  __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
+$app = new \Slim\Slim(
+	[
+		'view' => new \Slim\Views\Blade(),
+		'templates.path' => __DIR__ . '/../templates',
+	]);
+
+$view = $app->view();
+$view->parserOptions = [
+	'debug' => true,
+	'cache' => __DIR__ . '/../cache',
+];
+
+$app->get('/hello/:name', function ($name) use ($app) {
+	$app->render('master', [
+		'variable' => "Hello, $name",
+	]);
 });
-$app->run();
 
+$app->run();
