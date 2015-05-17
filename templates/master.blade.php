a3da880f181f4c67d7d3c4d55abda3fff366fc3a
Junior <ayijun@gmail.com>
2015-05-17 23:55:11 +0800

    testing blade raw print

--- templates/master.blade.php
+++ templates/master.blade.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <body>
+    	<h1>
         {{ $variable }}
+        </h1>
+        {{ '<p style="color:red">test</p>' }}
+        {!! '<p style="color:red">test</p>' !!}
     </body>
 </html>
